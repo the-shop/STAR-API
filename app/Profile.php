5fd3113c8a24ec1ef0fdae4d8a37ad9f1fdb0a53
Mladen <lotar.mladen@gmail.com>
2016-03-24 16:23:19 +0100

    token based auth for APIs and basic profile implementation

--- app/Profile.php
+++ app/Profile.php
@@ -1,17 +1,44 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: debeli
- * Date: 16.3.2016.
- * Time: 11:18
- */
+
 namespace App;
 
+use Illuminate\Support\Facades\Hash;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-
-use Jenssegers\Mongodb\Eloquent\Model;
-
-class Profile extends Model
+/**
+ * Class Profile
+ * @package App
+ */
+class Profile extends Eloquent implements AuthenticatableContract, CanResetPasswordContract
 {
-    protected $fillable =['user'];
+    use Authenticatable, CanResetPassword;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'email', 'password', 'trello', 'slack', 'github'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * Passwords must always be hashed
+     *
+     * @param $password
+     */
+    public function setPasswordAttribute($password)
+    {
+        // Let's hash the password when setting the attribute
+        $this->attributes['password'] = Hash::make($password);
+    }
 }
