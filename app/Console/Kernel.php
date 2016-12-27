212db76b7bfbcb3010a48e7aa4ed295dce74f0aa
Mladen Lotar <lotar.mladen@gmail.com>
2016-12-27 23:19:26 +0100

    Enable task reminder daily two times at 9:00 and 16:00

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        // Commands\Inspire::class,
         Commands\SprintReminder::class
     ];
 
@@ -25,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+         $schedule->command('sprint:remind')
+                  ->twiceDaily(9, 16);
     }
 }
