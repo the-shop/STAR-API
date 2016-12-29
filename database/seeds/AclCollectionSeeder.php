1bfa0c6884e2ebf666d02979cece8aba508d9455
Mladen Lotar <lotar.mladen@gmail.com>
2016-12-30 00:08:31 +0100

    Fix issues with seeder namespaces and linter

--- database/seeds/AclCollectionSeeder.php
+++ database/seeds/AclCollectionSeeder.php
@@ -1,43 +1,45 @@
 <?php
 
-namespace Illuminate\Support\Facades\DB;
+namespace
 
-use Illuminate\Database\Seeder;
-
-class AclCollectionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // delete records from acl collection
-        DB::collection('acl')->delete();
+    use Illuminate\Database\Seeder;
 
-        // insert records into acl collection
-        DB::collection('acl')->insert([
-            'name'   => 'standard',
-            'allows' => [
-                'GET' => [
-                    "api/v1/{resource}",
-                    "api/v1/{resource}/{id}",
-                    "api/v1/configuration",
-                    "api/v1/profiles",
-                    "api/v1/profiles/{profiles}",
-                    "api/v1/slack/users"
-                ],
-                'PUT' => [
-                    'api/v1/profiles/changePassword',
-                    'api/v1/profiles/{profiles}'
-                ],
-                'PATCH' => [
-                    'api/v1/profiles/{profiles}'
+    class AclCollectionSeeder extends Seeder
+    {
+        /**
+         * Run the database seeds.
+         *
+         * @return void
+         */
+        public function run()
+        {
+            // delete records from acl collection
+            DB::collection('acl')->delete();
+
+            // insert records into acl collection
+            DB::collection('acl')->insert([
+                'name'   => 'standard',
+                'allows' => [
+                    'GET' => [
+                        "api/v1/{resource}",
+                        "api/v1/{resource}/{id}",
+                        "api/v1/configuration",
+                        "api/v1/profiles",
+                        "api/v1/profiles/{profiles}",
+                        "api/v1/slack/users"
+                    ],
+                    'PUT' => [
+                        'api/v1/profiles/changePassword',
+                        'api/v1/profiles/{profiles}'
+                    ],
+                    'PATCH' => [
+                        'api/v1/profiles/{profiles}'
+                    ]
                 ]
-            ]
             ]);
 
-        $this->command->info("acl collection seeded!");
+            $this->command->info("acl collection seeded!");
+        }
     }
 }
