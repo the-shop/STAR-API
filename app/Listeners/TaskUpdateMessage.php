eb72825da21215f0f1b19f3f07b95a28d600b61f
mvoloder <marko.voloder@gmail.com>
2017-01-02 16:58:04 +0100

    XP event

--- app/Listeners/TaskUpdateMessage.php
+++ app/Listeners/TaskUpdateMessage.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use App\Events\TaskUpdate;
 use App\GenericModel;
+use App\Profile;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -34,30 +35,39 @@ class TaskUpdateMessage
 //        //estimated task time
 //        $estimate_help = $this->tasks->estimatedHours * 3600;
 
+        //task user id
+        $user_id = $this->tasks->task_history[0]['user'];
+
+        $profile = Profile::where('id', $user_id)->get();
+        var_dump($profile);
+
+        //avoid milliseconds
         $submitted = $this->tasks->due_date - intval($this->tasks->task_history[0]['timestamp'] / 1000);
 
         $coefficient = ($this->tasks->due_date - $submitted) / $this->tasks->due_date;
         $xp = $this->tasks->xp;
+
+
         switch ($coefficient) {
             case ($coefficient <= 0.75):
-                $xp += 3;
-                echo $xp;
+                $profile->xp += 3;
+                $profile->save();
                 break;
             case ($coefficient >= 0.75 && $coefficient <= 1):
-//                $xp = $this->tasks->xp;
-                echo $xp;
+                $profile->xp += 0;
+                $profile->save();
                 break;
             case ($coefficient >= 1.01 && $coefficient <= 1.1):
-                $xp -= 1;
-                echo $xp;
+                $profile->xp -= 1;
+                $profile->save();
                 break;
             case ($coefficient >= 1.11 && $coefficient <= 1.25):
-                $xp -= 2;
-                echo  $xp;
+                $profile->xp -= 2;
+                $profile->save();
                 break;
             case ($coefficient >= 1.26 && $coefficient < 1.4):
-                $xp -= 3;
-                echo $xp;
+                $profile->xp -= 3;
+                $profile->save();
                 break;
         }
     }
