fc735f9b0bf49a7cb811293ed84f08464fb2ca29
Josip Brčina <info1x2@gmail.com>
2017-01-16 15:53:35 +0100

    Setup user standard role upon registration, writing migration to loop through existing profiles and set role

---
+++ database/migrations/2017_01_16_144358_update_profile_role.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace {
+
+    use Illuminate\Database\Migrations\Migration;
+    use App\GenericModel;
+
+    class UpdateProfileRole extends Migration
+    {
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+        {
+            GenericModel::setCollection('profiles');
+            $profiles = GenericModel::all();
+            foreach ($profiles as $profile) {
+                if (empty($profile->role) && $profile->admin === true) {
+                    $profile->update([
+                        'role' => 'admin'
+                    ]);
+                } elseif (empty($profile->role) && $profile->employee === true) {
+                    $profile->update([
+                        'role' => 'employee'
+                    ]);
+                } elseif (empty($profile->role)) {
+                    $profile->update([
+                        'role' => 'standard'
+                    ]);
+                }
+            }
+        }
+
+        public function down()
+        {
+        }
+    }
+}
