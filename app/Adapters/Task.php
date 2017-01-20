67b71b550e37a3250a996aacb6744476472cffd0
Josip Brčina <info1x2@gmail.com>
2017-01-20 17:24:55 +0100

    Return task color status

--- app/Adapters/Task.php
+++ app/Adapters/Task.php
@@ -28,6 +28,42 @@ class Task implements AdaptersInterface
         $this->task->estimatedHours = sprintf('%.2f', $this->task->estimatedHours);
         $this->task->xp = sprintf('%.2f', $this->task->xp);
 
+        $taskStatus = $profilePerformance->perTask($this->task);
+
+        if (!empty($taskStatus)) {
+            $taskEstimatedSeconds = $mappedValues['estimatedHours'] * 60 * 60;
+            $taskDeliveredOnTime = $taskStatus[$this->task->owner]['workSeconds']
+            <= $taskEstimatedSeconds ? true : false;
+
+            // deadline in last 25% of the time of task
+            $lastQuarterOfTask = (25 / 100) * $taskEstimatedSeconds;
+
+            //generate task color status
+            if ($this->task->passed_qa === true && $taskDeliveredOnTime === true) {
+                $this->task->colorIndicator = 'Green';
+                return $this->task;
+            }
+
+            if ($this->task->passed_qa === true && $taskDeliveredOnTime === false) {
+                $this->task->colorIndicator = 'Red';
+                return $this->task;
+            }
+
+            if ($this->task->submitted_for_qa === true) {
+                $this->task->colorIndicator = 'Blue';
+                return $this->task;
+            }
+
+            if ($this->task->paused === true) {
+                $this->task->colorIndicator = 'Yellow';
+                return $this->task;
+            }
+
+            if (($taskEstimatedSeconds - $taskStatus[$this->task->owner]['workSeconds']) <= $lastQuarterOfTask) {
+                $this->task->colorIndicator = 'Orange';
+            }
+        }
+
         return $this->task;
     }
 }
