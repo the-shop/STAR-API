b2c27b214b357a0f05478a6eafbccb5fd1df9ed1
Josip Brčina <info1x2@gmail.com>
2017-01-23 12:35:01 +0100

    Small changes as requested

--- app/Adapters/Task.php
+++ app/Adapters/Task.php
@@ -32,37 +32,35 @@ class Task implements AdaptersInterface
 
         if (!empty($taskStatus)) {
             $taskEstimatedSeconds = $mappedValues['estimatedHours'] * 60 * 60;
-            $taskDeliveredOnTime = $taskStatus[$this->task->owner]['workSeconds']
-            <= $taskEstimatedSeconds;
+            $taskDeliveredOnTime = $taskStatus[$this->task->owner]['workSeconds'] <= $taskEstimatedSeconds;
 
             // deadline in last 25% of the time of task
             $lastQuarterOfTask = 0.25 * $taskEstimatedSeconds;
 
+            $colorIndicator = '';
             //generate task color status
-            if ($this->task->passed_qa === true && $taskDeliveredOnTime === true) {
-                $this->task->colorIndicator = 'green';
-                return $this->task;
+            if (($taskEstimatedSeconds - $taskStatus[$this->task->owner]['workSeconds']) <= $lastQuarterOfTask) {
+                $colorIndicator = 'orange';
             }
 
-            if ($this->task->submitted_for_qa === true) {
-                $this->task->colorIndicator = 'blue';
-                return $this->task;
+            if ($taskDeliveredOnTime === false) {
+                $colorIndicator = 'red';
             }
 
             if ($this->task->paused === true) {
-                $this->task->colorIndicator = 'yellow';
-                return $this->task;
+                $colorIndicator = 'yellow';
             }
 
-            if (($taskEstimatedSeconds - $taskStatus[$this->task->owner]['workSeconds']) <= $lastQuarterOfTask) {
-                $this->task->colorIndicator = 'orange';
+            if ($this->task->submitted_for_qa === true) {
+                $colorIndicator = 'blue';
             }
 
-            if ($this->task->passed_qa === false && $taskDeliveredOnTime === false) {
-                $this->task->colorIndicator = 'red';
-                return $this->task;
+            if ($this->task->passed_qa === true) {
+                $colorIndicator = 'green';
             }
         }
+
+        $this->task->colorIndicator = $colorIndicator;
 
         return $this->task;
     }
