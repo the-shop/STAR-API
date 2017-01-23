05a570d8ed0acfcbe44e30b2d85a1716e736ad17
Josip Brčina <info1x2@gmail.com>
2017-01-23 12:59:39 +0100

    Small change as requested

--- app/Adapters/Task.php
+++ app/Adapters/Task.php
@@ -30,6 +30,8 @@ class Task implements AdaptersInterface
 
         $taskStatus = $profilePerformance->perTask($this->task);
 
+        $colorIndicator = '';
+        
         if (!empty($taskStatus)) {
             $taskEstimatedSeconds = $mappedValues['estimatedHours'] * 60 * 60;
             $taskDeliveredOnTime = $taskStatus[$this->task->owner]['workSeconds'] <= $taskEstimatedSeconds;
@@ -37,7 +39,6 @@ class Task implements AdaptersInterface
             // deadline in last 25% of the time of task
             $lastQuarterOfTask = 0.25 * $taskEstimatedSeconds;
 
-            $colorIndicator = '';
             //generate task color status
             if (($taskEstimatedSeconds - $taskStatus[$this->task->owner]['workSeconds']) <= $lastQuarterOfTask) {
                 $colorIndicator = 'orange';
