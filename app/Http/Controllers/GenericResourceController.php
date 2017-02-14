d2e3ad19171ddf1912084af5a519b7a8e250ace7
Josip Brčina <info1x2@gmail.com>
2017-02-14 18:04:01 +0100

    Fixed loose search on generic resource controller

--- app/Http/Controllers/GenericResourceController.php
+++ app/Http/Controllers/GenericResourceController.php
@@ -39,7 +39,8 @@ class GenericResourceController extends Controller
                 'orderBy',
                 'orderDirection',
                 'offset',
-                'limit'
+                'limit',
+                'looseSearch'
             ];
 
             $operator = '=';
