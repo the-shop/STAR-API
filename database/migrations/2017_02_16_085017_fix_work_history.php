de5d8b2ab6de235782d23a9c3de6ea82f0f4012d
Mladen <lotar.mladen@gmail.com>
2017-02-16 09:56:22 +0100

    Add migration to fix task work history

---
+++ database/migrations/2017_02_16_085017_fix_work_history.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace {
+
+    use App\GenericModel;
+    use Illuminate\Database\Migrations\Migration;
+
+    class FixWorkHistory extends Migration
+    {
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+        {
+            GenericModel::setCollection('tasks');
+            $tasks = GenericModel::all();
+
+            foreach ($tasks as $task) {
+                $work = $task->work;
+                if (!$work) {
+                    $work = [];
+                }
+
+                $newWork = [];
+
+                foreach ($work as $userId => $history) {
+                    if (!isset($history['qa_in_progress'])) {
+                        $history['qa_in_progress'] = 0;
+                    }
+
+                    if (!isset($history['qa_total_time'])) {
+                        $history['qa_total_time'] = 0;
+                    }
+
+                    $newWork[$userId] = $history;
+                }
+
+                if ($newWork !== $work) {
+                    $task->work = $newWork;
+                    $task->save();
+                }
+            }
+        }
+
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            //
+        }
+    }
+
+}
